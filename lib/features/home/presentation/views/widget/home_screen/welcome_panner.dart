27146270ed0e71f6227aeae4ba21294891261b79
hello <abdelrahmantemsah30@gmail.com>
2026-01-29 15:19:23 +0200

    update WelcomePanner styling and improve text readability

--- lib/features/home/presentation/views/widget/home_screen/welcome_panner.dart
+++ lib/features/home/presentation/views/widget/home_screen/welcome_panner.dart
@@ -2,9 +2,9 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tabibak/core/constatnt/app_string.dart';
+import 'package:tabibak/core/extenstion/spacing.dart';
 import 'package:tabibak/core/theme/app_colors.dart';
 import 'package:tabibak/features/home/presentation/manager/home_provider/home_provider.dart';
-import 'package:tabibak/features/home/presentation/views/widget/home_screen/image_circle.dart';
 
 class WelcomePanner extends StatelessWidget {
   const WelcomePanner({super.key});
@@ -14,7 +14,7 @@ class WelcomePanner extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
+        color: Theme.of(context).colorScheme.primary,
         borderRadius: BorderRadius.circular(16),
       ),
       child: Consumer(builder: (context, ref, _) {
@@ -24,8 +24,6 @@ class WelcomePanner extends StatelessWidget {
 
         return Row(
           children: [
-            ImageCircle(urlImage: userModel?.image),
-            const SizedBox(width: 12),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,15 +31,15 @@ class WelcomePanner extends StatelessWidget {
                   Text(
                     "${AppStrings.welcome.tr()}, ${userModel != null ? userModel.name : ""} 👋",
                     style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.bold, color: AppColors.black),
+                        fontWeight: FontWeight.bold, color: AppColors.white),
                   ),
-                  const SizedBox(height: 4),
+                  5.hBox,
                   Text(
                     AppStrings.findBestDoctor.tr(),
                     style: Theme.of(context)
                         .textTheme
                         .titleSmall
-                        ?.copyWith(color: AppColors.black),
+                        ?.copyWith(color: AppColors.white),
                   ),
                 ],
               ),
