2084d05bc174483a467557a4cfba090e59fc24e2
hello <abdelrahmantemsah30@gmail.com>
2026-04-19 11:56:27 +0200

    set up notification with firebase

--- lib/core/services/app_service.dart
+++ lib/core/services/app_service.dart
@@ -1,5 +1,7 @@
 import 'package:tabibak/core/helper/dependancy_injection.dart';
 import 'package:tabibak/core/helper/shared_pref.dart';
+import 'package:tabibak/core/services/local_notification_services.dart';
+import 'package:tabibak/core/services/push_notification_service.dart';
 
 import 'env_service.dart';
 import 'subbase_service.dart';
@@ -11,7 +13,10 @@ class AppService {
     await EnvService.init();
     await SharedPrefsService.init();
     await SupabaseService.init();
-
+    await Future.wait([
+      LocalNotificationServices.init(),
+      PushNotificationService.init(),
+    ]);
     setupServiceLocator();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tabibak/firebase_options.dart';
 import 'package:tabibak/my_app.dart';
 
 import 'core/services/app_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
 
   await AppService.init();
@@ -17,10 +21,7 @@ void main() async {
       supportedLocales: const [Locale('en'), Locale('ar')],
       path: "assets/langs",
       fallbackLocale: Locale('ar'),
-      child: DevicePreview(
-        enabled: false,
-        builder: (context) => const MyApp(),
-      ),
+      child: const MyApp(),
     ),
   ));
 }
